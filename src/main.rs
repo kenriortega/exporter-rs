110812c388084a083d679c313286c962177b97e6
kenriortega <kenriortega@gmail.com>
2023-04-27 06:10:45 -0700

    feat: adding simple logic to read only metrics logs, and apply some refactor in this code.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use notify::{RecommendedWatcher, RecursiveMode, Watcher, Config, Event};
-use std::path::Path;
+use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 
 fn main() {
     let path = std::env::args()
@@ -32,13 +34,37 @@ fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
     Ok(())
 }
 
-fn event_parser(event: Event){
-    println!("changed: {:?}", event)
-    // parse the events related with log.file (should be Modify)?
-    // get paths and foreach all logs files and read content
-    // parse all lines (using patterns or regex) (format apache, nginx, IIS) also JSON?
-    // Use a database (like sqlite) to store last line check before close file (create struct)
-    // Send data to different sources (kafka, loki, postgresql, should be elastic)
-    // Create configuration file
-    // Create dockerfile for this app and convert to daemon set for k8s.
+fn event_parser(event: Event) {
+    let paths = event.paths;
+    // READ files from paths changed
+    read_file_log(paths);
+}
+
+fn read_file_log(paths: Vec<PathBuf>) {
+    // TODO: learn how to read all content in the file first time and then only read by last line (like and offset)
+    for path in paths.iter() {
+        println!("file to read {:?}", path.file_name());
+        match path.file_name() {
+            Some(name) => {
+                if name == "access.metrics.log" {
+                    let file = File::open(path).unwrap();
+                    let reader = BufReader::new(file);
+                    for (i, line) in reader.lines().enumerate() {
+                        // parse logfile to an struct
+                        parse_log_line(i, line.unwrap().to_owned());
+                    }
+                } else {
+                    println!(
+                        "This log file {}, don`t have an implemented parser for this moment",
+                        name.to_str().unwrap()
+                    );
+                }
+            }
+            None => {}
+        }
+    }
+}
+
+fn parse_log_line(i: usize, line: String) {
+    println!("number line: {}, content_log: {}", i, line);
 }
